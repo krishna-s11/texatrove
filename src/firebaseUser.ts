1ac62a8557b347ad066cd2de0617e587e01bcf3a
Krishna Saxena <66038375+krishna-s11@users.noreply.github.com>
2022-07-13 13:38:30 +0530

    Update firebaseUser.ts

--- src/firebaseUser.ts
+++ src/firebaseUser.ts
@@ -5,13 +5,13 @@ import "firebase/analytics";
 import "firebase/storage";
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyAwCBflZbXeRfoVa-a8r2pXP4eHQkaMCLQ",
-  authDomain: "texatrove-user.firebaseapp.com",
-  projectId: "texatrove-user",
-  storageBucket: "texatrove-user.appspot.com",
-  messagingSenderId: "31578782252",
-  appId: "1:31578782252:web:2c29ad0e57bad5a82f6f89",
-  measurementId: "G-9DSZBL621F",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID
 };
 
 firebase.initializeApp(FirebaseConfig);
